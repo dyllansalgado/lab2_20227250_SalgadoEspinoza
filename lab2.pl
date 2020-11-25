6702f56dc5e886e8a3c21ed944ba86ad8c69fdcb
dyllan salgado <dyllan.salgado@usach.cl>
2020-11-25 20:25:16 -0300

    primer commit.

---
+++ lab2.pl
@@ -0,0 +1,25 @@
+%Laboratorio numero 2 en Prolog Paradigmas de Programacion-
+%Autor: Dyllan Salgado 
+%Rut: 20227250-9
+%Fecha de inicio: 25-11-2020
+
+
+
+
+
+%TDA STACK
+%Es una lista de lista.
+
+createStackOverflow([[],[],[],[]]).
+
+
+
+%Pertenencias
+
+
+
+
+%selectores.
+stackGetUsuarios([Usuarios,Preguntas,Etiquetas,])
+
+
