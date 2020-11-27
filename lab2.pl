039e8c3dffa9503ef261354d7fa4ba274555fde2
dyllan salgado <dyllan.salgado@usach.cl>
2020-11-27 10:54:58 -0300

    aun no entiendo bien prolog, finde semana le pongo bueno

--- lab2.pl
+++ lab2.pl
@@ -8,18 +8,48 @@
 
 
 %TDA STACK
+%crearStack([dasd],[dasda],[dsadas],[dasda],S).
 %Es una lista de lista.
-
-createStackOverflow([[],[],[],[]]).
-
+crearStack(Usuarios, Preguntas,Respuestas,UsuarioLogeado,Salida):-
+    is_list(Usuarios),
+    is_list(Preguntas),
+    is_list(Respuestas),
+    is_list(UsuarioLogeado),
+    Salida=[Usuarios,Preguntas,Respuestas,UsuarioLogeado].
 
 
 %Pertenencias
 
+usuario(pedro,1234).
 
+agregarArticulo(L,Articulo,[Articulo,L]).
 
 
 %selectores.
-stackGetUsuarios([Usuarios,Preguntas,Etiquetas,])
+stackGetUsuarios([Usuarios,Preguntas,Etiquetas],Usuarios).
 
 
+/*
+crearLista(Usuarios, Preguntas,Respuestas,UsuarioLogeado,Salida):-
+    is_list(Usuarios),
+    is_list(Preguntas),
+    is_list(Respuestas),
+    is_list(UsuarioLogeado),
+    Salida=[Usuarios,Preguntas,Respuestas,UsuarioLogeado].
+
+usuario(["juanito","123"]).
+usuario(["ana","234"]).
+usuario(["pedro","345"]).
+
+ * agregar elemento al final de una lista
+
+agregarAlFinal([],E,[E]).
+agregarAlFinal([Anterior|Cola],E,[Anterior|NuevaCola]):-
+    agregarAlFinal(Cola,E,NuevaCola).
+
+/* probando
+agregarUsuario(Usuario,ListaEntrada,ListaSalida):-
+    nth0(0,ListaEntrada,UsuariosAntiguos),
+    Lista
+
+*/
